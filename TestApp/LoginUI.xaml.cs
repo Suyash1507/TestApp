acebca8a86dbcf3f829ec58cd14ffd06f960f73b
SUYASH <64803486+Suyash1507@users.noreply.github.com>
2022-01-07 22:27:42 +0530

    Add files via upload

---
+++ TestApp/LoginUI.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TestApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LoginUI : ContentPage
+    {
+        public LoginUI()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            bool isUsernameEmpty = string.IsNullOrEmpty(txtUsername.Text);
+            bool isPasswordEmpty = string.IsNullOrEmpty(txtPassword.Text);
+            if(isUsernameEmpty || isPasswordEmpty)
+            {
+                DisplayAlert("Oops..", "Username or Password is incorrect","Ok");
+            }
+            else
+            {
+                Navigation.PushAsync(new MainPage());
+            }
+        }
+
+        private void TapGestureRecognizer_Tapped(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new RegisterPage());
+        }
+    }
+}
---
+++ TestApp/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace TestApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OneTap_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new HomePage());
+        }
+
+        
+    }
+}
